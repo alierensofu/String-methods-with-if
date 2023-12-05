9a86717cb2760b0df0be597b147edb11ddba84a6
alierensofu <111981466+alierensofu@users.noreply.github.com>
2023-12-05 19:14:54 +0300

    string methods with if
    made in visual studio it is for udemy c# masterclass exam

---
+++ String methods with if/String methods with if/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1 = "";
+string SearchCharcter = "";
+Console.WriteLine("please enter a string here");
+s1 = Console.ReadLine();
+Console.WriteLine("please enter a character to search");
+SearchCharcter = Console.ReadLine();
+if (s1.Contains(SearchCharcter))
+{
+    Console.WriteLine("the characters index is {0}", s1.IndexOf(SearchCharcter));
+}
+else
+{
+    Console.WriteLine("please enter something contains in the string");
+}
+Console.WriteLine("please enter your first name");
+string FirstName, LastName;
+FirstName = Console.ReadLine();
+Console.WriteLine("please enter your lastname");
+LastName = Console.ReadLine();
+string FullName = FirstName + LastName;
+Console.WriteLine("Your full name is " + FullName);
+Console.ReadKey();
